ba84bcdde11f4d2030827c0dc860be8c37018623
Dan Allen <dan.j.allen@gmail.com>
2010-07-12 06:26:57 +0000

    rename

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# This script generates an archetype into target/archetype from the source project.
+#
+# Two optional commands can be provided (in the order listed):
+#
+# install - which will automatically install the generated archetype in the local repository
+# generate - generate a project from the archetype into target/generated-project
+#
+
+ARCHETYPE_BUILD_DIR=target/generated-sources/archetype
+ARCHETYPE_DIR=target/archetype
+ARCHETYPE_RESOURCES_DIR=$ARCHETYPE_DIR/src/main/resources/archetype-resources
+
+echo Generating archetype from project into $ARCHETYPE_BUILD_DIR...
+mvn clean archetype:create-from-project -Darchetype.properties=archetype.properties
+echo Relocating generated archetype project to $ARCHETYPE_DIR...
+rsync -az --exclude `basename $0` --exclude archetype-pom.xml $ARCHETYPE_BUILD_DIR/src $ARCHETYPE_DIR/
+cp -f archetype-pom.xml $ARCHETYPE_DIR/pom.xml
+mvn -f $ARCHETYPE_DIR/pom.xml clean
+echo Patching generated archetype...
+# could also use col -b
+sed -i 's;;;' $ARCHETYPE_RESOURCES_DIR/pom.xml
+sed -i 's;;;' $ARCHETYPE_RESOURCES_DIR/readme.txt
+sed -i 's;<name>jboss-javaee6-webapp-src</name>;<name>${name}</name>;' $ARCHETYPE_RESOURCES_DIR/pom.xml
+# could set the release version at this point, if specified
+if [ ! -z $1 ] && [ "$1" = "install" ]; then
+   echo Installing archetype...
+   shift
+   mvn -f $ARCHETYPE_DIR/pom.xml install
+fi
+
+if [ ! -z $1 ] && [ "$1" = "generate" ]; then
+   echo Generating project from archetype...
+   cd target
+   mvn archetype:generate -B -DarchetypeCatalog=local \
+      -DarchetypeArtifactId=jboss-javaee6-webapp -DarchetypeGroupId=org.jboss.weld.archetypes -DarchetypeVersion=1.0.1-SNAPSHOT \
+      -DartifactId=generated-project -DgroupId=com.acme -Dversion=1.0.0-SNAPSHOT -Dname="Java EE 6 Webapp"
+fi
---
+++ src/test/java/com/mycompany/MemberRegistrationTest.java
@@ -0,0 +1,51 @@
+package com.mycompany;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import static org.junit.Assert.*;
+import javax.inject.Inject;
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(Arquillian.class)
+public class MemberRegistrationTest
+{
+   @Deployment
+   public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(WebArchive.class, "test.war")
+         .addClasses(Member.class, MemberRegistration.class, MemberRepository.class, MemberRepositoryProducer.class)
+         .addPackage(Logger.class.getPackage())
+         .addWebResource("test-persistence.xml", "classes/META-INF/persistence.xml")
+         .addWebResource(new ByteArrayAsset(new byte[0]), "beans.xml");
+   }
+
+   @Inject MemberRegistration memberRegistration;
+
+   @Inject Logger log;
+
+   @Test
+   public void testRegister() throws Exception
+   {
+      Member newMember = memberRegistration.getNewMember();
+      newMember.setName("Jane Doe");
+      newMember.setEmail("jane@mailinator.com");
+      newMember.setPhoneNumber("2125551234");
+      memberRegistration.register();
+      assertNotNull(newMember.getId());
+      log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+   }
+
+   @Produces
+   public Logger produceLog(InjectionPoint injectionPoint)
+   {
+      return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+   }
+}
