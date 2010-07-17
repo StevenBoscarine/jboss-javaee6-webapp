a1f75d2f7b3452da86dcdbcab896cc3ad6b5910c
Steven Boscarine <stevenboscarine@gmail.com>
2010-07-17 18:19:10 +0000

    minor refactor, updated comments

---
+++ src/main/java/com/mycompany/JaxRsActivator.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * This empty, annotated {@link Application} class is the Java EE 6 "no XML"
+ * approach to activate JAX-RS. Resources are served from the servlet path
+ * specified in the @ApplicationPath annotation.
+ * 
+ * NOTE As of JBoss AS 6.0.0.M3, JAX-RS does not activate properly.
+ */
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application{/* left blank intentionally */}
